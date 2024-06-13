b57365c4793679e23b31fb72bb3f5266633f2c8c
bdimon <regsdimon-reg@yahoo.com>
2024-06-13 18:01:30 +0200

    added PMA

---
+++ plugin/dvb-core.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Dvb Core
+Plugin URI: http://dvb.info.com
+Description: Наш первый плагин по бронированнию.
+Version: 1.0
+Author: Dmytro
+Author URI: http://dvb.info
+License: GPLv2 or later
+Text Domain: dvb-core
+*/
